bfc5fd20464e5632e54dd57c98143b034bb01adf
Riza Khan <riza.khan@appnovation.com>
2021-08-02 16:55:20 -0400

    small adjuments

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,8 @@
 <?php
-
-// Database params
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'password');
 define('DB_NAME', 'mvc_framework');
-
 define('APPROOT', dirname(dirname(__FILE__)));
-define('URLROOT', 'localhost/mvc-framework');
+define('URLROOT', '/mvc-framework');
 define('SITENAME', 'MVC Framework');
