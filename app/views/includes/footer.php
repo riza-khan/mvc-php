f863d7af308f27ba91ce46f425813b6de3ddb203
Riza Khan <riza.khan@appnovation.com>
2021-08-02 16:57:59 -0400

    header and footer files created

---
+++ app/views/includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>Welcome to the PHP Framework</p>
+</footer>
+</body>
+
+</html>
---
+++ app/views/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo SITENAME ?></title>
+  <link rel="stylesheet" href="">
+</head>
+
+<body>
+  <header>
+    <ul>
+      <li><a href="<?php echo URLROOT ?>/">Home</a></li>
+      <li><a href="<?php echo URLROOT ?>/about">About</a></li>
+      <li><a href="<?php echo URLROOT ?>/contact">Contact</a></li>
+      <li><a href="<?php echo URLROOT ?>/privacy">Privacy</a></li>
+    </ul>
+  </header>
