d675bc7b91a20a70ef7a4dee39c98073e2cb07b4
Riza Khan <riza.khan@appnovation.com>
2021-08-02 17:00:26 -0400

    importing config earlier

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+// need this here because the configs are needed earlier
+require_once '../app/config/config.php';
 require_once '../app/views/includes/header.php';
 require_once '../app/require.php';
 require_once '../app/views/includes/footer.php';
